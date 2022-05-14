0128e9973f07a2a08735eb37909e77d69692d3b0
NEXOS123 <76531840+NEXOS123@users.noreply.github.com>
2022-05-14 19:20:43 +0100

    Add files via upload

---
+++ VedooElipse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VedooControls
+{
+    public class VedooElipse : Component
+    {
+        private Control hostControl;
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+            );
+        private Control _cntrl;
+        private int _CornerRadius = 30;
+
+        public Control TargetControl
+        {
+            get { return _cntrl; }
+            set
+            {
+                _cntrl = value;
+                try
+                {
+                    _cntrl.SizeChanged += (sender, eventArgs) => _cntrl.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, _cntrl.Width, _cntrl.Height, _CornerRadius, _CornerRadius));
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Elipse Control\nError at:\n-You need to select a target.\n\n"+ex.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+               
+            }
+        }
+
+        public int CornerRadius
+        {
+            get { return _CornerRadius; }
+            set
+            {
+                _CornerRadius = value;
+                if (_cntrl != null)
+                    _cntrl.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, _cntrl.Width, _cntrl.Height, _CornerRadius, _CornerRadius));
+            }
+        }
+    }
+}
